0b7d8c8d3d9acda5ae33a7c88c96ca316b4a6ce1
Shengwen Cheng <c.shengwen.tw@gmail.com>
2021-02-03 20:41:45 +0800

    complete identity matrix initialization of matrix inversion

--- src/matrix/matrix_standalone.c
+++ src/matrix/matrix_standalone.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <string.h>
 #include "robotics_math.h"
 #include "matrix.h"
@@ -83,6 +84,39 @@ void matrix_transpose(matrix_t *mat, matrix_t *mat_transposed)
 	}
 }
 
-int matrix_inverse(matrix_t *mat, matrix_t mat_inv)
+int matrix_inverse(matrix_t *mat, matrix_t *mat_inv)
 {
+	ASSERT(mat->row == mat->column);
+	ASSERT(mat->row == mat_inv->row);
+	ASSERT(mat->column == mat_inv->column);
+
+	uint32_t row_num    = mat->row;
+	uint32_t column_num = mat->column;
+
+	/*================================================*
+	 * initialize return matrix "mat_inv" to identity *
+	 *================================================*/
+	uint32_t r, c;	
+	uint32_t upper_num, lower_num;
+	float *inv_data_ptr = mat_inv->data;
+
+	uint32_t last_of_curr_row;
+	for(r = 0; r < mat_inv->row; r++) {
+		/* initialize lower triangle */
+		upper_num = row_num - r;
+		while(upper_num > 0) {
+			*inv_data_ptr++ = 0; //set value and move to next
+			upper_num--;	
+		}
+
+		/* initialize diagonal */
+		*inv_data_ptr++ = 1; //set value 1 and move to next
+
+		/* initialize upper triangle */
+		lower_num = r - 1;
+		while(lower_num > 0) {
+			*inv_data_ptr++ = 0; //set value and move to next
+			lower_num--;
+		}
+	}
 }
